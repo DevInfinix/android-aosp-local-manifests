65c3740c04856305f2ff333f3025f5e2310f33c8
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2024-07-12 16:18:22 +0530

    `bashupload` and `pixeldrain`

--- script.sh
+++ script.sh
@@ -10,12 +10,52 @@ rm -rf vendor/oneplus/sm8350-common
 rm -rf vendor/oplus/camera  
 rm -rf vendor/qcom/opensource/vibrator
 
+echo "========================================================================"
+echo "DELETED DIRECTORIES"
+echo "========================================================================"
+
 git clone https://github.com/DevInfinix/android-aosp-local-manifests --depth 1 -b 14-derp-bleeding-edge .repo/local_manifests
+
+echo "========================================================================"
+echo "CLONED REPOS"
+echo "========================================================================"
 
 /opt/crave/resync.sh
 
 source build/envsetup.sh
 
-lunch derp_ice-userdebug
+echo "========================================================================"
+echo "BUILDING........."
+echo "========================================================================"
 
+lunch derp_ice-userdebug
 mka bacon
+
+echo "========================================================================"
+echo "BUILT ROM SUCCESSFULLY!"
+echo "========================================================================"
+
+# Find the path of the zip file in the 'ice' folder
+ICE_FOLDER="ice"
+ZIP_FILE=$(find "$ICE_FOLDER" -name "*.zip" -type f -print -quit)
+
+# Check if a zip file was found
+if [ -n "$ZIP_FILE" ]; then
+    echo "Found zip file: $ZIP_FILE"
+
+    # Upload to bashupload.com
+    echo "Uploading to bashupload.com..."
+    curl bashupload.com -T "$ZIP_FILE"
+
+    # Upload to pixeldrain.com
+    echo "Uploading to pixeldrain.com..."
+    curl -T "$ZIP_FILE" https://pixeldrain.com/api/file/
+
+    echo "========================================================================"
+    echo "UPLOAD COMPLETED!"
+    echo "========================================================================"
+else
+    echo "========================================================================"
+    echo "No zip file found in the 'ice' folder."
+    echo "========================================================================"
+fi
