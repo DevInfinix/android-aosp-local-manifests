ac9012976958385c54e461e451d49aa8a9c9c141
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2024-07-15 04:44:37 +0530

    Clone `Vibrator` after Resync & drop `Zip Upload`

--- script.sh
+++ script.sh
@@ -14,9 +14,6 @@ echo "========================================================================"
 echo "DELETED DIRECTORIES"
 echo "========================================================================"
 
-# Clone Vibrator
-git clone https://github.com/DevInfinix/android_vendor_qcom_opensource_vibrator -b 14-derp-bleeding-edge vendor/qcom/opensource/vibrator
-
 # Clone local_manifests repository
 git clone https://github.com/DevInfinix/android-aosp-local-manifests --depth 1 -b 14-derp-bleeding-edge .repo/local_manifests
 if [ ! 0 == 0 ]
@@ -27,42 +24,26 @@ echo "========================================================================"
 echo "CLONED REPOS"
 echo "========================================================================"
 
+# Resync
 /opt/crave/resynctest.sh
+
+echo "========================================================================"
+echo "RESYNCED"
+echo "========================================================================"
+
+# Clone Vibrator
+git clone https://github.com/DevInfinix/android_vendor_qcom_opensource_vibrator -b 14-derp-bleeding-edge vendor/qcom/opensource/vibrator
+
+echo "========================================================================"
+echo "CLONED VIBRATOR"
+echo "========================================================================"
 
 echo "========================================================================"
 echo "BUILDING........."
 echo "========================================================================"
 
+# LUNCH
 source build/envsetup.sh
 lunch derp_ice-userdebug
 make installclean
 mka bacon
-
-echo "========================================================================"
-echo "BUILT ROM SUCCESSFULLY!"
-echo "========================================================================"
-
-# Find the path of the zip file in the 'ice' folder
-ICE_FOLDER="ice"
-ZIP_FILE=$(find "$ICE_FOLDER" -name "*.zip" -type f -print -quit)
-
-# Check if a zip file was found
-if [ -n "$ZIP_FILE" ]; then
-    echo "Found zip file: $ZIP_FILE"
-
-    # Upload to bashupload.com
-    echo "Uploading to bashupload.com..."
-    curl bashupload.com -T "$ZIP_FILE"
-
-    # Upload to pixeldrain.com
-    echo "Uploading to pixeldrain.com..."
-    curl -T "$ZIP_FILE" https://pixeldrain.com/api/file/
-
-    echo "========================================================================"
-    echo "UPLOAD COMPLETED!"
-    echo "========================================================================"
-else
-    echo "========================================================================"
-    echo "No zip file found in the 'ice' folder."
-    echo "========================================================================"
-fi
