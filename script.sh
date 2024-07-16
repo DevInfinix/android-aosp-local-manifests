1ca4adc59df7d0a5451dac6d8cfd4143b2d6ac42
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2024-07-16 19:19:50 +0530

    ice: Remove keys directory before cloning
    `script.sh`

--- script.sh
+++ script.sh
@@ -8,6 +8,7 @@ rm -rf hardware/oplus
 rm -rf device/oneplus
 rm -rf vendor/oneplus
 rm -rf vendor/oplus
+rm -rf vendor/derp/signing/keys
 rm -rf vendor/qcom/opensource/vibrator
 
 echo "========================================================================"
