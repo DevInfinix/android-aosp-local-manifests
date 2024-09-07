74f2494bfd77b9b2404aadec9da4b521fca54f31
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2024-09-07 16:06:43 +0530

    ice: script: Create keys directory manually

--- script.sh
+++ script.sh
@@ -60,6 +60,7 @@ fi
 
 echo "Cloning the repository..."
 git clone https://github.com/DevInfinix/devinfinix-aosp-roms-keys -b 14.0 temp-repo
+mkdir $DIRKEYS
 mv temp-repo/DerpFest-14/* "$DIRKEYS"
 rm -rf temp-repo
 
