ed7e1f9bcb593ddb74d7d3d84e62b68fdde51e1b
DevInfinix <singhsamarjeet2007@gmail.com>
2024-09-30 02:14:40 +0530

    ice: Welcome Blaze-15!

--- script.sh
+++ script.sh
@@ -8,8 +8,6 @@ rm -rf hardware/oplus
 rm -rf device/oneplus
 rm -rf vendor/oneplus
 rm -rf vendor/oplus
-rm -rf vendor/signing/yaap/keys
-
 
 echo "========================================================================"
 echo "DELETED DIRECTORIES"
@@ -17,7 +15,7 @@ echo "========================================================================"
 
 
 # Repo Init
-repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs --depth=1
+repo init -u https://github.com/ProjectBlaze/manifest -b 15 --git-lfs --depth=1
 
 echo "========================================================================"
 echo "REPO INITIALIZED"
@@ -25,7 +23,7 @@ echo "========================================================================"
 
 
 # Clone local_manifests repository
-git clone https://github.com/DevInfinix/android-aosp-local-manifests --depth 1 -b 15-yaap .repo/local_manifests
+git clone https://github.com/DevInfinix/android-aosp-local-manifests --depth 1 -b 15-blaze .repo/local_manifests
 if [ ! 0 == 0 ]
     then curl -o .repo/local_manifests https://github.com/DevInfinix/android-aosp-local-manifests.git
 fi
@@ -57,7 +55,7 @@ echo "========================================================================"
 
 # Clone Keys
 
-DIRKEYS="vendor/signing/yaap/keys/"
+DIRKEYS="vendor/blaze/signing/keys/"
 # Check if the directory exists
 if [ -d "$DIRKEYS" ]; then
     echo "Directory $DIRKEYS exists. Deleting it..."
@@ -68,28 +66,11 @@ else
 fi
 
 echo "Cloning the repository..."
-git clone https://github.com/DevInfinix/devinfinix-aosp-roms-keys --depth=1 -b 15.0-testing "$DIRKEYS"
+git clone https://github.com/DevInfinix/devinfinix-aosp-roms-keys --depth=1 -b 15.0-blaze "$DIRKEYS"
 
 echo "========================================================================"
 echo "CLONED KEYS"
 echo "========================================================================"
-
-
-# Set LD_LIBRARY_PATH (libgcc_s.so.1 linker)
-
-export LD_LIBRARY_PATH=/usr/aarch64-linux-gnu/bin:/$LD_LIBRARY_PATH
-
-# Temp Fix: Copy libgcc libs
-
-sudo mkdir -p /usr/lib/gcc/x86_64-linux-gnu/11
-sudo mkdir -p /usr/lib/gcc/x86_64-linux-gnu/13
-sudo cp -r /usr/aarch64-linux-gnu/lib/* /usr/lib/gcc/x86_64-linux-gnu/
-sudo cp -r /usr/aarch64-linux-gnu/lib/* /usr/lib/gcc/x86_64-linux-gnu/11
-sudo cp -r /usr/aarch64-linux-gnu/lib/* /usr/lib/gcc/x86_64-linux-gnu/13
-
-# Temp fix: Remove Seedvault
-
-rm -rf external/seedvault
 
 
 echo "========================================================================"
@@ -99,6 +80,6 @@ echo "========================================================================"
 
 # Lunch
 source build/envsetup.sh && \
-lunch yaap_ice-userdebug && \
+lunch blaze_ice-ap3a-userdebug && \
 make installclean ; \
-m yaap
+make bacon
