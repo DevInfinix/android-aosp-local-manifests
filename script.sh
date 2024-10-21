ec95bc7a7704859dc6124fbf785fe0b84a5557a7
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2024-10-21 16:02:36 +0530

    ice: script: minor fixes

--- script.sh
+++ script.sh
@@ -13,7 +13,6 @@ rm -rf packages/apps/ViPER4AndroidFX
 # Cleanup to fix SyncErrors raised during branch checkouts
 rm -rf platform/prebuilts
 rm -rf prebuilts
-rm -rf .repo/projects
 
 
 echo "========================================================================"
