b1b7b69d34189d921bbcebccf3a16261903bf684
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2024-10-31 23:49:38 +0530

    ice: script : Add as remote repo to fetch patches

--- script.sh
+++ script.sh
@@ -62,7 +62,8 @@ echo "========================================================================"
 # TEMP: Patch build/soong to allow oplus packages
 
 cd build/soong
-git fetch https://github.com/RisingTechOSS/android_build_soong fourteen
+git remote add ros https://github.com/RisingTechOSS/android_build_soong
+git fetch ros
 git cherry-pick 87df93b1ea08c751e2047b3d9e36bda487945e28
 cd ../..
 
